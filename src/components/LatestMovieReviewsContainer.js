ed49415e6d6624c3e5b56cb3656e5f235ca86a6d
alisabaj <alisabaj@gmail.com>
2019-12-16 17:09:35 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,37 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL)
+            .then(resp => resp.json())
+            .then(data => 
+                this.setState({
+                    reviews: data.results
+                })
+            )
+    }
+
+    mapReviews = () => {
+        return this.state.reviews.map(review => {
+            return <MovieReviews key={review.link.url} review={review}/>
+        })
+    }
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                <h1>Latest Movie Reviews:</h1>
+                {this.mapReviews()}
+            </div>
+        )
+    }
+}
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,22 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+    const { byline, critics_pick, headline, mpaa_rating, publication_date, summary_short, link } = props.review
+    return(
+        <div className="review-list">
+            <div className="review">
+                <h2>{headline} </h2>
+                <h4>By {byline || "anonymous"}, {publication_date}</h4>
+                <p>{summary_short}</p>
+                <p>Rated {mpaa_rating || "NR"}</p>
+                <p>Critics Pick? {critics_pick === 1 ? "Yes" : "No"}</p>
+                <p><a href={link.url}>{link.suggested_link_text}</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieReviews
+
+
